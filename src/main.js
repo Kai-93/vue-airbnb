ffe660f5da471e78f85df5ff2d25644ceff6db79
kaiser <765934690@qq.com>
2019-06-10 09:51:06 +0800

    remove local mock

--- src/main.js
+++ src/main.js
@@ -1,17 +1,16 @@
+/*
+ * @Author: Kaiser
+ * @Date: 2019-06-10 09:50:36
+ * @Last Modified by: Kaiser
+ * @Last Modified time: 2019-06-10 09:50:56
+ */
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import App from './App';
 import router from './router';
 import store from './store';
-// // 是否开启mock
-// const bOpenMock = false;
 
-// if (bOpenMock) {
-//   // 直接一般api
-//   // eslint-disable-next-line global-require
-//   require('./mock/mock');
-// }
 Vue.config.productionTip = false;
 
 window.$vue = new Vue({
--- src/store/states/ui_state.js
+++ src/store/states/ui_state.js
@@ -1,3 +1,9 @@
+/*
+ * @Author: Kaiser
+ * @Date: 2019-06-10 09:50:18
+ * @Last Modified by:   Kaiser
+ * @Last Modified time: 2019-06-10 09:50:18
+ */
 const state = {
   isLoadingShow: true,
   innerLoading: {
